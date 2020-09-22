1144672bfa2725a05c6a591cc75cd6e7ead99fce
Dante Beltran <dante.beltran15@gmail.com>
2020-09-22 18:25:12 -0400

    Finished Constructors

--- week2.cpp
+++ week2.cpp
@@ -1,11 +1,33 @@
 ﻿// week2.cpp : Defines the entry point for the application.
 //
+
 #include<iostream>
 
 using namespace std;
 
-int main()
-{
-	cout << "Hello CMake." << endl;
-	return 0;
+class Vehicle {
+private:
+	int numWheels;
+	int numDoors;
+
+
+public: 
+	Vehicle(int w, int d) {
+		numWheels = w;
+		numDoors = d;
+
+	}
+	Vehicle(int w): Vehicle(w,4) {
+		
+	}
+	Vehicle(): Vehicle(4) { 
+		
+	}
+
+
+};
+int main() {
+
+	cout << "Hello CMake" << endl;
+	return 0 ;
 }
